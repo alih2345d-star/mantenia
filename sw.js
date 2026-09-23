112806856baad785feadf5aa4a5961917cb43ae6
alih2345d-star <alih2345d@gmail.com>
2026-09-23 14:54:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "mantenia-pwa-v1";
+const CACHE = "mantenia-pwa-v2";
+
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE)
